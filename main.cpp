dbcc2ed01a0d0eb7645bc795031e6c9092ade3ba
leonardumale <leonardumale@gmail.com>
2026-03-11 19:47:33 -0700

    Implement ticket sales calculation and user input

--- main.cpp
+++ main.cpp
@@ -7,23 +7,25 @@ int main()
   const double ClassBCost = 12.00;
   const double ClassCCost = 9.00;
 
-  // TODO: Declare integer variables for tickets sold for each class
+int ticketsA, ticketsB, ticketsC;
+double totalA, totalB, totalC, total; 
+cout << "Enter the number of tickets sold for each class." << endl;
+cin >> ticketsA;
+cout << "Enter the number of Class B tickets sold: " << endl;
+cin >> ticketsB;
+cout << "Enter the number of Class C tickets sold: " << endl;
+cin >> ticketsC;
 
-  // TODO: Declare double variables for total sales per class and grand total
-
-  // TODO: Prompt user and read number of Class A tickets sold
-
-  // TODO: Prompt user and read number of Class B tickets sold
-
-  // TODO: Prompt user and read number of Class C tickets sold
-
-  // TODO: Calculate sales for each class
-
-  // TODO: Calculate total sales
+totalA = ticketsA * ClassACost;
+totalB = ticketsB * ClassBCost;
+totalC = ticketsC * ClassCCost;
+total = totalA + totalB + totalC;
 
   cout << setprecision(2) << fixed;
   cout << "Tickets sold for Class A " << ticketsA << " Sales Price for A   " << totalA << endl;
   cout << "Tickets sold for Class B " << ticketsB << " Sales Price for B   " << totalB << endl;
   cout << "Tickets sold for Class C " << ticketsC << " Sales Price for C   " << totalC << endl;
-  cout << "Total Sales                              " << total << endl;
+  cout << "Total Sales" << total << endl;
+  return 0;
 }
+
